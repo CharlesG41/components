1116be948cb43067578e2ae27b1001ad25a63957
Charles Giguere <charles-edouard.giguere@monespaceproprio.com>
2023-01-24 19:05:11 -0500

    start

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Hero1 />
+    <DragAndDrop />
+  </div>
+</template>
+
+<script>
+import Hero1 from './components/Hero/Hero1.vue'
+import DragAndDrop from './components/DragAndDrop.vue'
+
+export default {
+  components: {
+    Hero1,
+    DragAndDrop
+  }
+}
+</script>
---
+++ src/components/Hero/Hero1.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="p-24">
+        <div>
+            <h1>Titre du hero</h1>
+            <h2>Sous-titre du hero</h2>
+            <p>Texte du hero</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    components: [
+
+    ],
+    props: {
+
+    }
+}
+</script>
